60de450bff43a9f7b03b758ed7413f250aaed112
otmaneee <oelkhemmar@gmail.com>
2021-09-22 17:01:49 +0100

    adding tasks to firebase

--- src/containers/ToDoList/BasicToDoList.jsx
+++ src/containers/ToDoList/BasicToDoList.jsx
@@ -7,66 +7,72 @@ import {
   buttonTexts,
 } from "../../constants";
 import CustomButton from "../../components/CustomButton";
-import { addTask , removeTask,pushTask } from "./action";
-import { useDispatch , useSelector} from "react-redux";
+import { addTask, removeTask, pushTask } from "./action";
+import { useDispatch, useSelector } from "react-redux";
 import { listMission } from "./constants";
+import { execute } from "../../services/request";
+import { collections } from "../../constants";
 
 const BasicToDoList = () => {
   const [task, setTask] = useState("");
-  const [counter,setCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
   const dispatch = useDispatch();
-  const todoList = useSelector((state)=>state.tasksState.taskList);
-  useEffect(()=>{
-    dispatch(pushTask(listMission))
-  },[]);
+  const todoList = useSelector((state) => state.tasksState.taskList);
+  useEffect(() => {
+    dispatch(pushTask(listMission));
+  }, []);
   const handleChange = (e) => {
-      setTask(e.target.value);
-  }
+    setTask(e.target.value);
+  };
   const handleClick = () => {
-      if(!task){
-          alert("Add a task");
-          return;
-      }
-      // setTodoList([...todoList,{id:counter,text:task}]);
-      dispatch(addTask({id:counter,text:task}));
-      setCounter(counter+1);
-      setTask("");
-  }
-   const deleteLastElement = () =>{
-     dispatch(removeTask());
-  }
+    if (!task) {
+      alert("Add a task");
+      return;
+    }
+    // setTodoList([...todoList,{id:counter,text:task}]);
+    dispatch(addTask({ id: counter, text: task }));
+    execute(collections.todo).create({ id: counter, text: task });
+    setCounter(counter + 1);
+    setTask("");
+  };
+  const deleteLastElement = () => {
+    dispatch(removeTask());
+  };
   return (
     <div>
-      <h1 style={{textAlign:"center",color:"red"}}>To-do List</h1>
-    <div className="todoContainer">
-      <CustomInput
-        placeholder={textInputProps.placeholder.toDoList}
-        label={textInputProps.label.toDoList}
-        onChange={handleChange}
-        value = {task}
-      />
-      <CustomButton
-        text={buttonTexts.toDoList}
-        color={bootstrapButtonVariant.danger}
-        onClick={handleClick}
-      />
-      <CustomButton
-        text={buttonTexts.delete}
-        color={bootstrapButtonVariant.outlineWarning}
-         onClick={deleteLastElement}
-      />
-      <br></br>
-      <br></br>
-      <h1>Tasks</h1>
-      <ul>
+      <h1 style={{ textAlign: "center", color: "red" }}>To-do List</h1>
+      <div className="todoContainer">
+        <CustomInput
+          placeholder={textInputProps.placeholder.toDoList}
+          label={textInputProps.label.toDoList}
+          onChange={handleChange}
+          value={task}
+        />
+        <CustomButton
+          text={buttonTexts.toDoList}
+          color={bootstrapButtonVariant.danger}
+          onClick={handleClick}
+        />
+        <CustomButton
+          text={buttonTexts.delete}
+          color={bootstrapButtonVariant.outlineWarning}
+          onClick={deleteLastElement}
+        />
+        
+        <br></br>
+        <br></br>
+        <h1>Tasks</h1>
+        <ul>
           {todoList.map((item) => (
             <div>
-              <li>{item.id} ----- {item.text}</li>
-               <CustomButton color={bootstrapButtonVariant.success} text={"✓"} />
+              <li>
+                {item.id} ----- {item.text}
+              </li>
+              <CustomButton color={bootstrapButtonVariant.success} text={"✓"} />
             </div>
           ))}
-      </ul>
-    </div>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/services/request.js
+++ src/services/request.js
@@ -3,19 +3,19 @@ export const execute = (provider) =>{
     const db = firebase.collection(provider);
 
      return{
-        getAll = () => {
+        getAll : () => {
             return db;
           },
           
-         create = (data) => {
+         create : (data) => {
             return db.add(data);
           },
           
-         update = (id, value) => {
+         update : (id, value) => {
             return db.doc(id).update(value);
           },
           
-         remove = (id) => {
+         remove : (id) => {
             return db.doc(id).delete();
                 }
      }
